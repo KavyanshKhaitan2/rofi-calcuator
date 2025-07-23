1cd7db8d0ae11c5d50803f24432d1159baa90cd7
KavyanshKhaitan2 <kavyanshkhaitan11@gmail.com>
2025-07-23 16:55:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# rofi-calculator
+Simple calculator built in Python using Rofi.
+![image](./image.png)
+
+This project is licensed under MIT.
---
+++ main.py
@@ -0,0 +1,34 @@
+from rofi import Rofi
+import math
+import time
+r = Rofi(location=0)
+
+rofi_args = ['-theme', '/home/user/rofi-calculator/calc.rasi']
+
+command_history = []
+
+
+# # <for_debugging>
+# eq = r.text_entry("Eval", message=(
+#             "[Esc] to go back\n\n"
+#             "Command History:\n"
+#             +"\n".join(f"Line {i}" for i in range(1, 6))
+#         ), allow_blank=True, rofi_args=rofi_args)
+# exit()
+# # </for_debugging>
+
+while True:
+    try:
+        eq = r.text_entry("Eval", message=(
+            "[Esc] to go back\n\n"
+            "Command History:\n"
+            +("\n".join(command_history) if command_history else "[None]")
+        ), rofi_args=rofi_args)
+        print(eq)
+        if eq is None:
+            break
+        print(eq)
+        res = eval(eq, globals={"__builtins__": None, "math": math})
+        command_history.append(f"{eq} = {res}")
+    except Exception as e:
+        r.error(f"Error!\n\n{e}\n\nPress [RETURN] to continue...", rofi_args=rofi_args)
